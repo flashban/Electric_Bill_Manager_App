8d704350a35213568395e9a8e829b7536d72a217
Abhishek Sarangi <abhishek.s3@TELXSIDS.NET>
2022-07-16 20:22:31 +0530

    added get support link

--- server/app.js
+++ server/app.js
@@ -1,24 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const PORT = 3001
+const PORT = 5000;
 
-app.get('/', (req, res) => {
+app.get('/bills', (req, res) => {
     fs.readFile(`${__dirname}/ElectricBillApi/billapi.json`, "utf8", (err, data) => {
         res.send(data);
     })
-})
-
-app.get('/:id', (req, res) => {
-    res.send(req.params.id);
-})
-
-app.get('/:id/:title', (req, res) => {
-    res.send(req.params);
-})
-
-app.get('/:id/:title/:day', (req, res) => {
-    res.send(req.query);
 })
 
 app.listen(PORT, (req, res) => {
