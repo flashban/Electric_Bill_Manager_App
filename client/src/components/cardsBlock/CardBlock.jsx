410086f83505cf96dd61070bbac303d73eabcd80
Abhishek Sarangi <abhishek.s3@TELXSIDS.NET>
2022-07-16 20:25:48 +0530

    get request link done

--- client/src/components/cardsBlock/CardBlock.jsx
+++ client/src/components/cardsBlock/CardBlock.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './cardBlock.css';
 
 const CardBlock = () => {
+  const [data, setData] = useState([]);
+
+  const billData = async () => {
+    try {
+      const res = await fetch("/bills", { 
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      const bill = await res.json();
+      
+      setData(bill);
+
+      if (!res.status === 200) {
+        const error = new Error(res.error);
+        throw error;
+      }
+
+    } catch (error) {
+      console.log("Error fetching card data: " + error);
+    }
+  };
+
+  useEffect(() => {
+    billData();
+  }, []);
+
   return (
-    <div className='cardBlock__body'>CardBlock</div>
+    <div className='cardBlock__body'>
+      {data.map((ele, index) => { 
+        return(
+        <div className='cards__box' key={index}>
+          <div><h1>Month: {ele.month}</h1></div>
+          <div><h2>Bill Amount: {ele.amount} Rupees</h2></div>
+          <div><h3>Payment Status: {ele.payStatus}</h3></div>
+          <div><h3>Mode of Payment: {ele.modeOfPayment}</h3></div>
+        </div>) 
+      })}
+    </div>
   )
 }
 
